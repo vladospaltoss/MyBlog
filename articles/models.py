98bb430b868c20da67b7e778d2cd097ae7037cbe
vladospaltoss <shuraour@gmail.com>
2023-01-25 17:03:21 +0300

    third_commit

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Articles(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     body = models.TextField()
 
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Articles
+from django.views.generic import ListView, DetailView
+from .forms import UserForm
 
 # Create your views here.
+
+class ArticleListView(ListView):
+    model = Articles
+    template_name = 'home.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Articles
+    template_name = 'detail.html'
+    context_object_name = 'details'
+
+
+def form_page(request):
+    form = UserForm()
+    dict_obj = { 'form' : form}
+    return render(request, './form_page.html', dict_obj)
+
+def thanks_page(request):
+    if request.POST:
+        title = request.POST['title']
+        body = request.POST['body']
+        element = Articles(title = title, body = body)
+        element.save()
+        return render(request, './thanks.html')
+    else:
+        return render(request, './thanks.html')
